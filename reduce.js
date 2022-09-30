796a8960b23557bd1a7dd48d76451496bbfdbf59
Javier Quiroga <javierquirogae@gmail.com>
2022-09-30 09:49:00 -0400

    formating

--- reduce.js
+++ reduce.js
@@ -1,5 +1,6 @@
 /*
-Write a function called extractValue which accepts an array of objects and a key and returns a new array with the value of each object at the key.
+Write a function called extractValue which accepts an array of objects and a key and 
+returns a new array with the value of each object at the key.
 
 Examples:
     const arr = [{name: 'Elie'}, {name: 'Tim'}, {name: 'Matt'}, {name: 'Colt'}]
@@ -9,7 +10,10 @@ Examples:
 function extractValue(arr, key) {}
 
 /*
-Write a function called vowelCount which accepts a string and returns an object with the keys as the vowel and the values as the number of times the vowel appears in the string. This function should be case insensitive so a lowercase letter and uppercase letter should count
+Write a function called vowelCount which accepts a string and 
+returns an object with the keys as the vowel and the values as the number of times the vowel appears in the string. 
+
+This function should be case insensitive so a lowercase letter and uppercase letter should count
 
 Examples:
     vowelCount('Elie') // {e:2,i:1};
@@ -22,7 +26,8 @@ Examples:
 function vowelCount(str) {}
 
 /*
-Write a function called addKeyAndValue which accepts an array of objects and returns the array of objects passed to it with each object now including the key and value passed to the function.
+Write a function called addKeyAndValue which accepts an array of objects and 
+returns the array of objects passed to it with each object now including the key and value passed to the function.
 
 Examples:
     const arr = [{name: 'Elie'}, {name: 'Tim'}, {name: 'Matt'}, {name: 'Colt'}];
@@ -39,7 +44,11 @@ Examples:
 function addKeyAndValue(arr, key, value) {}
 
 /*
-Write a function called partition which accepts an array and a callback and returns an array with two arrays inside of it. The partition function should run the callback function on each value in the array and if the result of the callback function at that specific value is true, the value should be placed in the first subarray. If the result of the callback function at that specific value is false, the value should be placed in the second subarray. 
+Write a function called partition which accepts an array and a callback and 
+returns an array with two arrays inside of it. The partition function should run the callback function on each value in the array 
+and if the result of the callback function at that specific value is true, the value should be placed in the first subarray. 
+
+If the result of the callback function at that specific value is false, the value should be placed in the second subarray. 
 
 Examples:
     
